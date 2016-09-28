736405dded1dc5c7e10c7c8774dc805d9327e0e3
Kihoon Jung <dare0021@kaist.ac.kr>
2016-09-28 17:11:34 +0900

    Changed the project info to Bork instead of TestProject

--- Bork/MainWindow.xaml.cs
+++ Bork/MainWindow.xaml.cs
@@ -15,7 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TestProject
+namespace Bork
 {
     /// <summary>
     /// MainWindow.xaml에 대한 상호 작용 논리
