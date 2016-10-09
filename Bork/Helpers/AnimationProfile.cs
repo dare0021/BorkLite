8456ac3d5de7be454bd339e5736792fbae1e6ce6
Kihoon Jung <dare0021@kaist.ac.kr>
2016-10-09 18:42:56 +0900

    Started work on animation

---
+++ Bork/Helpers/AnimationProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bork.Helpers
+{
+    public class AnimationProfile
+    {
+        public AnimationProfile()
+        {
+            IsAnimated = false;
+
+            //TODO: figure out how to fit this in to the image store structure
+            // problem: loading a bunch of images safely and painlessly
+        }
+
+        public bool IsAnimated { get; set; }
+        public int Framerate { get; set; }
+    }
+}
