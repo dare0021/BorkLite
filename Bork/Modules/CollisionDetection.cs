725838a08f78df5b4f8fc943f4c2b6e4a2c85758
Kihoon Jung <dare0021@kaist.ac.kr>
2017-01-02 22:11:39 +0900

    Fixed CollisionDetection.rayTrace() having flipped target x and y

--- Bork/Modules/CollisionDetection.cs
+++ Bork/Modules/CollisionDetection.cs
@@ -242,10 +242,13 @@ namespace Bork.Modules
         static public GameDisplayObject rayTrace(GameDisplayObject from, float rayLength, out Vec2 intersection, bool checkProjectilesAlso = false)
         {
             Vec2 origin = from.getPosition();
-            Vec2 target = origin + new Vec2(rayLength * Math.Cos(from.getRotation() * Math.PI / 180),
-                                            rayLength * Math.Sin(from.getRotation() * Math.PI / 180));
+            Vec2 target = origin + new Vec2(rayLength * Math.Sin(from.getRotation() * Math.PI / 180),
+                                            rayLength * Math.Cos(from.getRotation() * Math.PI / 180));
             intersection = null;
             var candidateList = new List<Pair<GameDisplayObject, Vec2>>();
+            Console.WriteLine("ROT: " + from.getRotation());
+            Console.WriteLine("Origin: " + origin);
+            Console.WriteLine("Target: " + target);
 
             foreach (var child in shipList)
             {
